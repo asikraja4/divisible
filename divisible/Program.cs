cd85c9ae03e37dc77b0077b69564812bb3e2583f
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:34:02 +0530

    Add project files.

---
+++ divisible/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisible
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 9 == 0)
+            {
+                Console.WriteLine("Divisible by 9");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible by 9");
+            }
+        }
+    }
+}
